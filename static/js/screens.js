45499c8f86c6bccacc4892c9f90cfd92d58032e3
rafapolo <rafael@tacticaltech.org>
2015-04-30 12:13:35 +0200

    edited content and test-me opens in new tab

--- static/js/screens.js
+++ static/js/screens.js
@@ -4,113 +4,117 @@ screens = {
 		'Default CA/cert': [
 			{
 				'src': 'certcheck_chromium_msauthority01.png',
-				'label': 'Description 1'
+				'label': 'Valid Certificate details'
 			}
 		],
 		'Default CA, similar cert': [
 			{
 				'src': 'certcheck_chromium_mrsimillar01.png',
-				'label': 'Description 2'
+				'label': 'Similar Certificate details'
 			}
 		],
 		'Similar CA, similar cert': [
 			{
 				'src': 'certcheck_chromium_simillarjunior01.png',
-				'label': 'Description 3'
+				'label': 'Valid Similar Certificate details'
 			}
 		],
 		'Self-signed cert': [
 			{
 				'src': 'certcheck_chromium_drselfsigned01.png',
-				'label': 'Description 4'
+				'label': 'Chrome reaction'
 			},{
 				'src': 'certcheck_chromium_drselfsigned02.png',
-				'label': 'Description 5'
+				'label': 'Chrome reaction details'
 			},{
 				'src': 'certcheck_chromium_drselfsigned03.png',
-				'label': 'Description 6'
+				'label': 'Chrome URL bar reaction'
 			},{
 				'src': 'certcheck_chromium_drselfsigned04.png',
-				'label': 'Description 7'
+				'label': 'Certificate details'
 			}
 		],
 		'Faked cert and CA': [
 			{
 				'src':'certcheck_chromium_unknownvalid01.png',
-				'label': 'Description 8'
+				'label': 'Chrome reaction'
 			},{
 				'src':'certcheck_chromium_unknownvalid02.png',
-				'label': 'Description 9'
+				'label': 'Chrome reaction details'
 			},{
 				'src':'certcheck_chromium_unknownvalid03.png',
-				'label': 'Description 10'
+				'label': 'Chrome URL bar reaction'
 			},{
 				'src':'certcheck_chromium_unknownvalid04.png',
-				'label': 'Description 11'
+				'label': 'Certificate details'
 			}
 		],
 		'A valid but untrusted CA, real cert': [
 			{
 				'src': 'certcheck_chromium_jackmiddleattacer01.png',
-				'label': 'Description 12'
+				'label': 'Chrome reaction'
 			},{
 				'src': 'certcheck_chromium_jackmiddleattacer02.png',
-				'label': 'Description 13'
+				'label': 'Chrome reaction details'
 			},{
 				'src': 'certcheck_chromium_jackmiddleattacer03.png',
-				'label': 'Description'
+				'label': 'Chrome URL bar reaction'
 			},{
 				'src': 'certcheck_chromium_jackmiddleattacer04.png',
-				'label': 'Description 14'
+				'label': 'Certificate details'
 			}
 		]
 	},
 	'Firefox': {
 		'Default CA/cert': [
+			{
+				'src':'d1.png',
+				'label': 'Valid Certificate'
+			}
 		],
 		'Default CA, similar cert': [
 			{
-				'src':'d2.png',
-				'label': 'Description 15'
-			},{
 				'src':'sm2.png',
-				'label': 'Description 16'
+				'label': 'Firefox reaction'
+			},{
+				'src':'d2.png',
+				'label': 'Firefox add exception to Certificate'
 			}
 		],
 		'Similar CA, similar cert': [
 			{
-				'src':'d3.png',
-				'label': 'Description 17'
-			},{
 				'src':'sm3.png',
-				'label': 'Description 18'
+				'label': 'Firefox reaction'
+			},{
+				'src':'d3.png',
+				'label': 'Firefox add exception to Certificate'
 			}
 		],
 		'Self-signed cert': [
 			{
-				'src':'d4.png',
-				'label': 'Description 19'
-			},{
 				'src':'sm4.png',
-				'label': 'Description 20'
+				'label': 'Firefox reaction'
+			},{
+				'src':'d4.png',
+				'label': 'Firefox add exception to Certificate'
 			}
 		],
 		'Faked cert and CA': [
 			{
-				'src':'d5.png',
-				'label': 'Description 21'
-			},{
 				'src':'sm5.png',
-				'label': 'Description 22'
+				'label': 'Firefox reaction'
+			},{
+				'src':'d5.png',
+				'label': 'Firefox add exception to Certificate'
 			}
 		],
 		'A valid but untrusted CA, real cert': [
 			{
-				'src':'d6.png',
-				'label': 'Description 23'
-			},{
 				'src':'sm6.png',
-				'label': 'Description 24'
+				'label': 'Firefox reaction'
+			},{
+				'src':'d6.png',
+				'label': 'Firefox add exception to Certificate'
 			}		
 		]
 	}
